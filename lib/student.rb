f53591d0fc01b7f316e72268ea2069b71464ec9e
Sam Markham <sam.e.markham@gmail.com>
2019-07-30 19:28:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,11 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each {|student| student = Student.new}
+    student = Student.new
+    students_array.each do |student_mini_hash|
+      student_mini_hash.each {|key, value| student.send(("#{key}="), value)}
+    end 
+    #{:name => "Abby Smith", :location => "Brooklyn, NY", :profile_url => "url"}
   end
 
   def add_student_attributes(attributes_hash)
