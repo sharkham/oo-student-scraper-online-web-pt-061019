6683a887194725cb07636b633f8db1f9393d8b36
Sam Markham <sam.e.markham@gmail.com>
2019-07-30 19:35:25 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,6 +12,7 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student|
       student = Student.new
+      binding.pry 
       student.each {|key, value| student.send(("#{key}="), value)}
       @@all << student
     end
